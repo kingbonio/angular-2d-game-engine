afb936655b5000987b7be3b24857789edd6787d4
Adam Kissaglis <adamkissaglis@hotmail.co.uk>
2020-01-12 20:05:24 +0000

    Remove starting items from game

--- src/app/game/shared/services/area-config-provider.service.ts
+++ src/app/game/shared/services/area-config-provider.service.ts
@@ -39,8 +39,8 @@ export class AreaConfigProviderService implements OnInit {
     // this.inventoryManagerService.addItemToInventory(potions.smallHealthPotion);
     // this.inventoryManagerService.addItemToInventory(potions.smallHealthPotion);
     this.inventoryManagerService.addItemToInventory(potions.smallHealthPotion);
-    this.inventoryManagerService.addItemToInventory(potions.invisiblityPotion);
-    this.inventoryManagerService.addItemToInventory(weapons.sword);
+    // this.inventoryManagerService.addItemToInventory(potions.invisiblityPotion);
+    // this.inventoryManagerService.addItemToInventory(weapons.sword);
     // this.inventoryManagerService.addItemToInventory(potions.smallDamageBuff);
     // this.inventoryManagerService.addItemToInventory(potions.largeHealthBuff);
     // this.inventoryManagerService.addItemToInventory(weapons.sword);
