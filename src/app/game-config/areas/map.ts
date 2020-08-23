f78b3885e7c6583bc9e6b498c9c28775b14b9857
Adam Kissaglis <adamkissaglis@hotmail.co.uk>
2020-08-23 18:19:43 +0100

    Config_Tidy - Update maps for area exits

--- src/app/game-config/areas/map.ts
+++ src/app/game-config/areas/map.ts
@@ -18,25 +18,25 @@ export default {
       3: {
             north: { direction: Direction.N, destination: 4, status: AreaExitStatus.closed, itemReferenceNeeded: "", keyColourNeeded: "" },
             east: null,
-            south: { direction: Direction.S, destination: 2, status: AreaExitStatus.open, itemReferenceNeeded: "", keyColourNeeded: "" },
+            south: { direction: Direction.S, destination: 2, status: AreaExitStatus.closed, itemReferenceNeeded: "", keyColourNeeded: "" },
             west: null,
       } as IAreaExits,
       4: {
             north: { direction: Direction.N, destination: 5, status: AreaExitStatus.closed, itemReferenceNeeded: "", keyColourNeeded: "" },
             east: null,
-            south: { direction: Direction.S, destination: 3, status: AreaExitStatus.open, itemReferenceNeeded: "", keyColourNeeded: "" },
+            south: { direction: Direction.S, destination: 3, status: AreaExitStatus.closed, itemReferenceNeeded: "", keyColourNeeded: "" },
             west: null,
       } as IAreaExits,
       5: {
             north: { direction: Direction.N, destination: 6, status: AreaExitStatus.locked, itemReferenceNeeded: "65a25063-4303-47b6-bdbe-0ce239396e0d", keyColourNeeded: "red" },
             east: null,
-            south: { direction: Direction.S, destination: 4, status: AreaExitStatus.open, itemReferenceNeeded: "", keyColourNeeded: "" },
+            south: { direction: Direction.S, destination: 4, status: AreaExitStatus.closed, itemReferenceNeeded: "", keyColourNeeded: "" },
             west: null,
       } as IAreaExits,
       6: {
             north: null,
             east: null,
-            south: { direction: Direction.S, destination: 5, status: AreaExitStatus.open, itemReferenceNeeded: "65a25063-4303-47b6-bdbe-0ce239396e0d", keyColourNeeded: "red" },
+            south: { direction: Direction.S, destination: 5, status: AreaExitStatus.locked, itemReferenceNeeded: "65a25063-4303-47b6-bdbe-0ce239396e0d", keyColourNeeded: "red" },
             west: null,
       } as IAreaExits,
 };
