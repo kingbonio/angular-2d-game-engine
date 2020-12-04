4e4ad7f60652e581b94554c35957e90dcabc7808
Adam Kissaglis <adamkissaglis@hotmail.co.uk>
2020-12-04 16:03:05 +0000

    Window_Scaling - Updated meta viewport data based on device

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 import { fromEvent } from 'rxjs/observable/fromEvent';
 import { Subscription } from 'rxjs/Subscription';
 import { GameSettingsService } from './shared/services/game-settings.service';
@@ -15,10 +16,22 @@ export class AppComponent implements OnInit, OnDestroy {
 
     constructor(
         private userInputService: UserInputService,
-        public gameSettingsService: GameSettingsService
+        public gameSettingsService: GameSettingsService,
+        private meta: Meta
     ) { }
 
     ngOnInit() {
+
+        console.log(window.navigator.userAgent);
+        console.log(screen);
+        if (screen.height < 763 && (screen.orientation.angle === 90 || screen.orientation.angle === -90)) {
+            this.meta.removeTag("name=viewport");
+            this.meta.addTag({
+                content: "width=device-width,initial-scale=0.45,maximum-scale=1,user-scalable=no",
+                name: "viewport"
+            });
+        }
+        console.log(this.meta.getTag("name=viewport"));
 
         // Listen for keyboard input events
         this.userInputSubscription = fromEvent(document, 'keydown').subscribe(($e: KeyboardEvent) => {
